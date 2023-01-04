09e68bfa370f0bb970cc13e62ecc2f8c5b7e247a
Vincent Lim <vincentlimjingen@gmail.com>
2023-01-04 16:36:37 +0800

    Add outgoing webhook for slash command interactions

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ In this game, players are randomly assigned roles that belong to the villager or
 
 The goal is for the villagers to identify and eliminate all the werewolves before they are killed by the werewolves and the goal of the werewolf is to eliminate the villagers.
 
+## Setting Up Project
+Create a config.json file
+```json
+{
+	"token": "your-token-goes-here",
+	"appId": "your-application-id-goes-here"
+}
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
+
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//
+// });
+
+app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), (req, res) => {
+    const interaction = req.body;
+    const { data } = interaction;
+
+    // Different slash commands
+    switch (data?.name) {
+        case "role":
+            res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: `Your role is ${"seer"}`,
+                    flags: 1 << 6
+                },
+            });
+            return
+    }
+});
+
+app.listen(3000, () => {
+    console.log('server started');
+});
